42e098fd733395a8029bbb41e312e6de3e703c6c
siddhant singh rajpoot <siddhantsinghrajpoot91@gmail.com>
2025-12-26 12:53:57 +0530

    setting up new config server

--- backend/services/event-intake-service/src/main/java/com/hxi/event_intake_service/kafka/EventProducer.java
+++ backend/services/event-intake-service/src/main/java/com/hxi/event_intake_service/kafka/EventProducer.java
@@ -1,39 +1,16 @@
 package com.hxi.event_intake_service.kafka;
 
-import com.hxi.event_intake_service.model.EventDocument;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class EventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    private static final String TOPIC = "user-events";
-
-    public void publishEvent(EventDocument event) {
-
-        Map<String, Object> payload = new HashMap<>();
-        payload.put("eventId", event.getEventId());
-        payload.put("sessionId", event.getSessionId());
-        payload.put("userId", event.getUserId());
-        payload.put("page", event.getPage());
-
-        // ✅ FIX: convert enum → string
-        payload.put("eventType", event.getEventType().name());
-
-        payload.put("metadata", event.getMetadata());
-        payload.put("timestamp", event.getTimestamp().toString());
-
-        kafkaTemplate.send(TOPIC, event.getEventId(), payload);
-
-        log.info("Kafka event published: {}", event.getEventId());
+    public void publish(String topic, Object payload) {
+        kafkaTemplate.send(topic, payload);
     }
 }
